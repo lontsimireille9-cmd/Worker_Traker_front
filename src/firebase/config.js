1fc60571885738fc4aa1581a389d5248acab8101
Aaron Ndemmenou <lontsimireille9@gmail.com>
2026-09-16 15:52:19 +0100

    Prevent blank screen when Firebase env is missing

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBkJWDa-9xtQ9ToJPXQaEgSP0hPgAgv9xM",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "workforce-tracker-5b5d3.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "workforce-tracker-5b5d3",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "workforce-tracker-5b5d3.appspot.com",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "290516265382",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:290516265382:web:23f0b9ff918023626bc767",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-CT5J3D0D6R",
 };
 
 const app = initializeApp(firebaseConfig);
